6a13b060afa9aa6aaa72d33301eb5a1ebb83620d
shahinrahmannaim <shahinrahman1000@gmail.com>
2024-07-08 17:46:40 +0200

    DESIEM COMMIT

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -16,7 +16,7 @@ class RecipeController extends AbstractController
     #[Route('/recette', name: 'recipe.index')]
     public function index(Request $request): Response
     {
-      return new Response('Recettes');
+      return $this->render('recipe/index.html.twig'); 
         
     }
    
@@ -26,9 +26,15 @@ class RecipeController extends AbstractController
     #[Route('/recette/{slug}-{id}', name: 'recipe.show',requirements:['id'=>'\d+','slug'=>'[a-z0-9-]+'])]
     public function show(Request $request, string $slug, int $id): Response
     {
-        return $this->json([
-            'slug'=>$slug
-        ]);
+        return $this->render('recipe/show.html.twig',[
+        'slug'=>$slug,
+        'id'=>$id,
+        'person'=>[
+            'firstame'=>'shaihn',
+            'lastname'=>'rahman'
         
+        ]
+        
+        ]);
     }
 }
